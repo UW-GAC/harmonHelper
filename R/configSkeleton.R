c13021c27df066305e01d61fc8f1fead169c3e42
Jai Broome <jbroome@uw.edu>
2017-03-28 13:28:38 -0700

    Update configSkeleton to accomodate database changes

--- R/configSkeleton.R
+++ R/configSkeleton.R
@@ -1,63 +1,83 @@
-#' Get encoded values by trait ID
-#' @param db A database connection
+#' Construct the skeleton for the XML configuration file for the harmonization batch. Takes named lists as arguments
 #' @param dbgap_trait_accessions A vector of dbGaP variable accessions, as integers
 #' @param file File path and filename for exported XML config file
 #' @import magrittr
 #' @import XML
 #' @export
-configSkeleton <- function(db, dbgap_trait_accessions, file = "RENAME_ME_config.xml"){
-  trait_ids <- dbGetTraitInfoByAccession(db, dbgap_trait_accessions) %>%
-    select(source_trait_id, study_name) %>%
-    plyr::dlply("study_name") %>%
-    lapply(function(x) x[,1])
 
-  # Input
 
-  input_node <- xmlNode("input")
-  for(study in names(trait_ids)){
-    node <- xmlNode("input_unit", attrs=c(unit_id=study)) %>%
-      addChildren(kids = list(xmlNode("custom_function", value="")))
+configSkeleton <- function(source_trait_ids,
+                           age_trait_ids,
+                           harmon_functions,
+                           name,
+                           qc_doc,
+                           output_dir = "",
+                           output_prefix = "output",
+                           description = "",
+                           data_type = "",
+                           unit){
 
-    kid_nodes <- lapply(trait_ids[[study]], function(x) {
-      xmlNode("source_trait_id", value=x)
+    stids <- source_trait_ids
+    atids <- age_trait_ids
+    hfs <- harmon_functions
+
+    # Error messages
+    if (sum(!(names(stids) %in% names(atids)))){
+        stop("The names of source_trait_ids and age_trait_ids don't match")
+    } else if (sum(!(names(stids) %in% names(hfs)))){
+        stop("The names of source_trait_ids and harmon_functions don't match")
+    } else if (!all(sapply(list(stids, atids, hfs), length) == length(stids))){
+        stop("source_trait_ids, age_trait_ids and harmon_functions are not all the same length")
     }
-    )
-    node %<>% addChildren(kids = kid_nodes)
-    input_node %<>% addChildren(kids = list(node))
-  }
 
-  # Output
+    # Input
+    input_node <- xmlNode("input")
+    for(hu in names(stids)){
+        node <- xmlNode("input_unit", attrs = c(unit_id = hu)) %>%
+            addChildren(kids = list(xmlNode("custom_function", value = hfs[[hu]])))
 
-  output_node <- xmlNode("output")
-  output_node <- addChildren(output_node, kids = list(
-    xmlNode("output_directory", value="")
-  ))
+        stid_node <- lapply(stids[[hu]], xmlNode, name = "source_trait_id")
+        atid_node <- lapply(atids[[hu]], xmlNode, name = "age_trait_id")
 
-  output_node <- addChildren(output_node, kids = list(
-    xmlNode("output_prefix", value="output")
-  ))
+        node %<>% addChildren(kids = c(stid_node, atid_node))
+        input_node %<>% addChildren(kids = list(node))
+    }
 
-  # Metadata
+    # Output
 
-  metadata_node <- xmlNode("metadata")
+    output_node <- xmlNode("output")
+    output_node %<>% addChildren(kids = list(xmlNode("output_directory",
+                                                     value=output_dir)))
 
-  target_node <- xmlNode("target")
-  target_node <- addChildren(target_node, kids = list(
-    xmlNode("name", value = ""),
-    xmlNode("description", value = ""),
-    xmlNode("data_type", value = ""),
-    xmlNode("unit", value = "")
-  ))
+    output_node %<>% addChildren(kids =
+                       list(xmlNode("output_prefix", value=output_prefix)))
 
-  metadata_node <- addChildren(metadata_node, kids = list(target_node))
+    # Metadata
 
-  #Config
-  xml_config <- xmlNode("config")
-  xml_config <- addChildren(xml_config, kids = list(
-    input_node,
-    metadata_node,
-    output_node
-  ))
+    metadata_node <- xmlNode("metadata")
 
-  saveXML(xml_config, file = file)
+    target_node <- xmlNode("target")
+    target_kids <- list(xmlNode("name", value = name),
+                      xmlNode("description", value = description),
+                      xmlNode("data_type", value = data_type))
+    if (hasArg(unit)){
+        target_kids %<>% c(list(xmlNode("unit", value = unit)))
+    }
+
+
+    target_node %<>% addChildren(kids = target_kids)
+
+    metadata_node %<>% addChildren(kids = list(target_node))
+
+    if (hasArg(qc_doc)){
+      metadata_node %<>% addChildren(kids = list(xmlNode("qc_document", value = qc_doc)))
+    }
+
+
+    # Config
+    xml_config <- xmlNode("config")
+    config_kids <- list(input_node,  metadata_node, output_node)
+    xml_config %<>% addChildren(kids = config_kids)
+
+    return(xml_config)
 }
