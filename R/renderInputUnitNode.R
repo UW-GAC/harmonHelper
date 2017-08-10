e16a8b22745ae515d073fef7801fb844b4126b5d
Jai Broome <jb@jaibroome.com>
2017-08-10 11:34:31 -0700

    Add renderInputUnitNode
    This function will construct a input unit node used in the configuration
    files for phenotype harmonization. It is intended to be used with
    forthcoming functions to build the config file in pieces

---
+++ R/renderInputUnitNode.R
@@ -0,0 +1,44 @@
+#' Builds an input unit node for a phenotype harmonization configuration file
+#' @param unit_name Name of the harmonization unit
+#' @param stids A vector of source_trait_ids
+#' @param atids A vector of source_trait_ids of phenotypes used to calculate age 
+#' @param htsids A vector of harmonized_trait_set_ids 
+#' @param btids A vector of source_trait_ids used for separating subjects/observations into batches
+#' @param hf Path to the harmonization function for this unit 
+
+renderInputUnitNode <- function(unit_name, 
+                                stids = NULL, 
+                                atids = NULL, 
+                                htsids = NULL, 
+                                btids = NULL, 
+                                hfs){
+    if (all(is.null(stids), is.null(atids), is.null(htsids), is.null(btids))){
+        stop("Please supply source_trait_ids for at least one argument")
+    }
+
+    node <- xmlNode("input_unit", attrs = c(unit_id = unit_name)) %>%
+        addChildren(kids = list(xmlNode("custom_function", value = hfs)))
+
+    if (!is.null(stids)){
+        stid_node <- lapply(stids, xmlNode, name = "source_trait_id")
+        node %<>% addChildren(kids = stid_node)
+    }
+    
+    if (!is.null(atids)){
+        atid_node <- lapply(atids, xmlNode, name = "age_trait_id")
+        node %<>% addChildren(kids = atid_node)
+    }
+
+    if (!is.null(htsids)){
+        htsid_node <- lapply(htsids, xmlNode, name = "harmonized_trait_set_id")
+        node %<>% addChildren(kids = htsid_node)
+    }
+    
+    if (!is.null(btids)){
+        btid_node <- lapply(btids, xmlNode, name = "batch_trait_id")
+        node %<>% addChildren(kids = btid_node)
+    }
+    
+    return(node)
+}
+
