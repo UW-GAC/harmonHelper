b16bf9624f322e02e6eddcdb083bc8260aa6a6b8
Jai Broome <jb@jaibroome.com>
2017-09-15 14:58:09 -0700

    Add N to QC violin plots
    Move .ggVar inside qcPlot to get around NSE

--- R/ggVar.R
+++
@@ -1,4 +0,0 @@
-.ggVar <- function(x){
- return(data.frame(y = min(x) - 0.15 * abs(min(x)), 
-                   label = round(var(x, na.rm = T), 2)))
-}
--- R/qcPlot.R
+++ R/qcPlot.R
@@ -3,86 +3,93 @@
 #' @param outcome name in dat for the quantitative trait
 #' @param covarsF names in dat for covariates that are to be factors
 #' @param covarsN names in dat for other covariates
-#' @param byVar grouping variable of interest
+#' @param byVar grouping variables of interest
 #' @param type type of plot: should be "box" or "violin"
 #' @param subj.include subject IDs for subjects to include; if NULL, all subjects in dat will be used
-#' @param fw Variable to facet by
 #' @param title brief text to prepend to automatic title
 #' @export
 #' @import ggplot2
 #' @import dplyr
 #' @import magrittr
 qcPlot <- function(dat,
-                    outcome,
-                    covarsF,
-                    covarsN,
-                    byVar,
-                    type,
-                    subj.include = NULL,
-                    fw,
-                    title = NULL){ 
+                   outcome,
+                   covarsF,
+                   covarsN,
+                   byVar,
+                   type,
+                   subj.include = NULL,
+                   title = NULL){ 
 
 # type = "box" yields box plots with sample sizes recorded below the box plots
 # type = "violin" yields violin plots with variances recorded below the violin plots
 # returns ggplot object
 
-  covars <- c(covarsF,covarsN)
-  if (!"data.frame" %in% class(dat)) stop("data must be data.frame")
-  if (!all(c(outcome,covars,byVar) %in% names(dat))) stop("some designated variable is not in data")
-  if (!type %in% c("box","violin")) stop("type must be box or violin")
+    covars <- c(covarsF, covarsN)
+    if (!"data.frame" %in% class(dat)) stop("data must be data.frame")
 
-  if (!is.element(class(dat[,outcome]),c("numeric","integer"))) stop("outcome must be numeric")
+    if (!all(c(outcome, covars, byVar) %in% names(dat))){
+        stop("some designated variable is not in data")
+    }
 
-  vars <- c(covarsF,byVar)
-  for(v in vars){
-    dat[,v] <- as.factor(dat[,v])
-  }
+    if (!type %in% c("box", "violin")) stop("type must be box or violin")
+
+    if (!is.element(class(dat[,outcome]), c("numeric", "integer"))){
+        stop("outcome must be numeric")
+    }
+
+    vars <- c(covarsF, byVar)
+    for(v in vars){
+        dat[, v] <- as.factor(dat[, v])
+    }
  
-  if (is.null(subj.include)) subj.include <- dat$topmed_subject_id
+    if (is.null(subj.include)) subj.include <- dat$topmed_subject_id
 
-  dat %<>% filter(topmed_subject_id %in% subj.include)
+    dat %<>% filter(topmed_subject_id %in% subj.include)
 
-  model <- paste(outcome, "~",paste(covars,collapse = " + "))
-  mod <- lm(model,dat)
-  dat$resid <- mod$residuals
+    model <- paste(outcome, "~", paste(covars,collapse = " + "))
+    mod <- lm(model, dat)
+    dat$resid <- mod$residuals
 
-  rm <- "resid"
-  mn <- min(dat[,rm], na.rm = T)
-  mx <- max(dat[,rm],na.rm = T)
-  yL <- mn-0.15*abs(mn)
+    rm <- "resid"
+    mn <- min(dat[, rm], na.rm = T)
+    mx <- max(dat[, rm], na.rm = T)
+    yL <- mn - 0.15 * abs(mn)
 
-  if (!is.null(title)){
-     title  <- paste0(title," : residuals by ",byVar,"\nmodel = ",model)
-  } else {
-     title <- paste0("residuals by ",byVar,"\nmodel = ",model)
-  }
-cat(paste("plotting", title, "\n\n"))
+    gtitle <- paste0("residuals by ", byVar, "\nmodel = ", model)
 
-  g <- ggplot(dat, aes_string(x = byVar,y = rm)) + 
-         ggtitle(title)
-         ylim(yL,mx) 
+    if (!is.null(title)){
+         gtitle <- paste(title, gtitle, sep = ": ")
+    } 
 
-   if (hasArg(fw)){
-     g <- g + facet_wrap(fw, ncol = 1)
+cat(paste("plotting", gtitle, "\n\n"))
+
+    g <- ggplot(dat, aes_string(x = byVar,y = rm)) + 
+        ggtitle(gtitle) +
+        ylim(yL,mx) 
+
+    if (length(byVar) > 1){
+        fw <- byVar[2:length(byVar)]
+            g <- g + facet_wrap(fw, ncol = 1) 
    }
 
   if (type == "box"){
-      g <- g + geom_boxplot(varwidth = TRUE) + 
-        geom_text(aes(label = prettyNum(..count.., big.mark = ",")), 
-                  y = yL, stat = "count", vjust = -1.2, color = "black",size = 4) 
+      g <- g + geom_boxplot(varwidth = TRUE) +
+          geom_text(aes(label = prettyNum(..count.., big.mark = ",")), 
+                    y = yL, stat = "count", vjust = -1.2, 
+                    color = "black", size = 4)
   } else {
-      g <- g + geom_violin()
-      if (hasArg(fw)){
-          g <- g + stat_summary(fun.data = .ggVar, geom = "text")
-
-      } else {
-          vr <- signif (tapply(dat[,rm],dat[,byVar],var),4)
-          vr <- vr[!is.na(vr)]
-          nvr <- length(vr)
-          g <- g +
-            annotate("text",x = 1:nvr, y = rep(yL,nvr), label = vr,vjust = -1.2, 
-                     color = "black",size = 4) 
-      }
+    .ggVar <- function(x){ 
+        # As a work around to issues with NSE in ggplot2, this function is 
+        # is defined here with the value for yL hardcoded in
+        return(data.frame(y = yL, 
+            label = paste0("(",
+                           prettyNum(length(x), big.mark = ","),
+                           "; ",
+                           prettyNum(round(var(x, na.rm = T), 2), big.mark = ","),
+                           ")")))
+    }
+      g <- g + geom_violin() + 
+          stat_summary(fun.data = .ggVar, geom = "text")
    }
   return(g)
 }
